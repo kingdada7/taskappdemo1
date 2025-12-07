344b7103dbf527ef3de1a811c940cf59d2cb3c9c
kingdada7 <toluwadada7@gmail.com>
2025-12-07 15:09:57 +0100

    setting up the dependecies and middleware ,routes, start server

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,31 @@
 import express from 'express';
-
+import cors from 'cors';
+import connectDB from './config/db.js';
+import dotenv from 'dotenv';
 
 
 const app = express();
+dotenv.config();
+
+// Middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// cors middleware
+app.use(cors());
+
+//db connection
+connectDB();
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+// START SERVER
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
