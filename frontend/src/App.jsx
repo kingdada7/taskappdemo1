41b3e5dab944ff6d93c3e4842e702b38ed1832e0
kingdada7 <toluwadada7@gmail.com>
2025-12-07 10:55:21 +0100

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-center text-blue-600">
+        Hello, Tailwind CSS with Vite and React!
+      </h1>
+    </div>
+  )
+}
+
+export default App
