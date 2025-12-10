088d46d23ad78459b1841063733d53fa664f432f
kingdada7 <toluwadada7@gmail.com>
2025-12-10 12:40:24 +0100

    creating register user in user controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,46 @@
+import userModel from "../model/userModel";
+import bycrpt from "bcryptjs";
+import validator from "validator";
+import jwt from "jsonwebtoken";
+const JWT_SECRET = process.env.JWT_SECRET || "your jwt secret key ";
+const createToken = (userId) =>
+  jwt.sign({ id: userId }, JWT_SECRET, { expiresIn: TOKEN_EXPIRES });
+//register user
+export async function registerUser(req, res) {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res
+      .status(400)
+      .json({ success: false, message: "All fields are required" });
+  }
+  if (!validator.isEmail(email)) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Invalid email format" });
+  }
+  if (password.length < 8) {
+    return res.status(400).json({
+      success: false,
+      message: "Password must be at least 8 characters long",
+    });
+  }
+
+  try {
+    if (await userModel.findOne({ email })) {
+      return res
+        .status(409)
+        .json({ success: false, message: "Email already in use" });
+    }
+    const hashed = await bycrpt.hash(password, 10);
+    const user = await user.create({ name, email, password: hashed });
+    const token = createToken(user._id);
+    res.staus(201).json({
+      success: true,
+      token,
+      user: { name: user.name, email: user.email, id: user._id },
+    });
+  } catch (error) {
+    console.log("Error registering user:", error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+}
