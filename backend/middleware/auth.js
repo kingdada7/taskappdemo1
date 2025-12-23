b4cfbe685f82be354ecbb22c898d662144ef8efe
kingdada7 <toluwadada7@gmail.com>
2025-12-23 03:18:17 +0100

    adding the authmiddleware to private links /protected

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../model/userModel";
+import User from "../model/userModel.js";
 
 const JWT_SECRET = process.env.JWT_SECRET || "your jwt secret key ";
 
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,20 @@
-import express from 'express';
-import { loginUser, registerUser } from '../controllers/userController';
+import express from "express";
+import {
+  loginUser,
+  registerUser,
+  getCurrentUser,
+  updateProfile,
+  updatePassword,
+} from "../controllers/userController";
+import authMiddleware from "../middleware/auth.js";
 
 const userRouter = express.Router();
 
 // Route for user registration public links
-userRouter.post ('/register',registerUser);
-userRouter.post('/login',loginUser);
+userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
 
 //protected routes private links
-userRouter.get('/me', getCurrentUser);
-userRouter.put('/profile',updateProfile);
-userRouter.put('/password',updatePassword);
+userRouter.get("/me", authMiddleware, getCurrentUser);
+userRouter.put("/profile", authMiddleware, updateProfile);
+userRouter.put("/password", authMiddleware, updatePassword);
