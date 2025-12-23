61d6845a5d746c53c0971c987af53ee9d6e15247
kingdada7 <toluwadada7@gmail.com>
2025-12-23 03:37:28 +0100

    added export to the userRoutes and imported it in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import connectDB from './config/db.js';
-import dotenv from 'dotenv';
-
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 dotenv.config();
@@ -19,13 +19,15 @@ app.use(cors());
 connectDB();
 
 // Routes
-app.get('/', (req, res) => {
-    res.send('API is running...');
+app.use("/api/user", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
 // START SERVER
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,7 +5,7 @@ import {
   getCurrentUser,
   updateProfile,
   updatePassword,
-} from "../controllers/userController";
+} from "../controllers/userController.js";
 import authMiddleware from "../middleware/auth.js";
 
 const userRouter = express.Router();
@@ -18,3 +18,5 @@ userRouter.post("/login", loginUser);
 userRouter.get("/me", authMiddleware, getCurrentUser);
 userRouter.put("/profile", authMiddleware, updateProfile);
 userRouter.put("/password", authMiddleware, updatePassword);
+
+export default userRouter;
