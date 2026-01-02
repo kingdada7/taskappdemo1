54b42d3365fd2e52674d00ca3aca249b3cddaf1d
kingdada7 <toluwadada7@gmail.com>
2026-01-02 13:02:53 +0100

    creating the task model / creating a new task

---
+++ backend/controllers/taskControler.js
@@ -0,0 +1,21 @@
+import Task from "../model/taskModel";
+
+// Create a new task
+export const createTask = async (req, res) => {
+  try {
+    const { title, description, priority, dueDate, completed } = req.body;
+    const task = new Task({
+      title,
+      description,
+      priority,
+      dueDate,
+      completed: completed === "yes" || completed === true,
+      owner: req.user.id,
+    });
+
+    const saved = await task.save();
+    res.status(201).json({ success: true, task: saved });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
--- backend/model/taskModel.js
+++ backend/model/taskModel.js
@@ -6,12 +6,15 @@ const taskSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+
     description: { type: String, default: "" },
+
     priority: {
       type: String,
       enum: ["Low", "Medium", "High"],
       default: "Low",
     },
+
     dueDate: { type: Date, required: true },
 
     owner: {
@@ -19,13 +22,15 @@ const taskSchema = new mongoose.Schema(
       ref: "User",
       required: true,
     },
+
     completed: { type: Boolean, default: false },
 
     createdAt: { type: Date, default: Date.now },
   },
+
   { timestamps: true }
 );
 
-const Task = mongoose.models.Tasks || mongoose.model("Tasks", taskSchema);
+const Task = mongoose.models.Tasks || mongoose.model("Task", taskSchema);
 
 export default Task;
